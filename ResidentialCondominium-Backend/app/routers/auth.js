7037838d5fa17ad1a3ac6726d0b8b394aa9735c3
Qdev1 <duongbqhe150006@fpt.edu.vn>
2023-10-26 12:49:09 +0700

    update code #B030

--- ResidentialCondominium-Backend/app/routers/auth.js
+++ ResidentialCondominium-Backend/app/routers/auth.js
@@ -7,5 +7,6 @@ router.post("/login", authController.login);
 router.post("/forgot-password", authController.forgotPassword);
 router.post("/reset-password", authController.resetPassword);
 router.post('/register-personal-info', registrationController.registerPersonalInfo);
+router.post('/notifications', authController.createNotification);
 
 module.exports = router;
