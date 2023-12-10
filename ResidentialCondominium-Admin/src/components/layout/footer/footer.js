b0f22713b9c735d5c7104d27679207d9acfe1ece
vietanh281 <hoangvietanh281@gmail.com>
2023-12-10 22:14:09 +0700

    update code

--- ResidentialCondominium-Admin/src/components/layout/footer/footer.js
+++ ResidentialCondominium-Admin/src/components/layout/footer/footer.js
@@ -9,7 +9,7 @@ function _Footer(){
         <Footer style={{
             marginLeft: 230,
             textAlign: 'center'}}>
-            Copyright@ 2023 Created by team ResidentialCondominium
+            Copyright@ 2023 Created by team CondoOperationsManagement
         </Footer>
     );
 }
